f71338c02b0da93421aa7a53cf77c978161f033c
martin <martin@DELL-SPARE4.ticra.com>
2026-01-26 16:20:22 +0100

    Database connection and simple data fetching

---
+++ Database/db.js
@@ -0,0 +1,66 @@
+import { MongoClient } from 'mongodb';
+
+// Enviromental variables
+const username = encodeURIComponent(process.env.MONGO_USER);
+const password = encodeURIComponent(process.env.MONGO_PASSWORD);
+
+// Database URL
+const uri = `mongodb+srv://${username}:${password}@telemetry.tgndpzv.mongodb.net/?retryWrites=true&w=majority`;
+
+// Attributes
+let client;
+let db;
+
+// Connect to Mongodb
+const connectDB = async () => {
+  if (db) {
+    return db;
+  }
+
+  try {
+    // Set client to database
+    client = new MongoClient(uri);
+
+    // Check connction
+    await client.connect();
+
+    // Sets db
+    db = client.db('gui_event_db');
+    console.log('MongoDB Atlas connected');
+
+    // Return db
+    return db;
+
+    // Error catch
+  } catch (err) {
+    console.error('MongoDB connection failed:', err.message);
+    process.exit(1);
+  }
+};
+
+// Fetch 5 latest from Mongodb
+const getFiveLatest = async () => {
+  try {
+    // Connects database
+    const database = await connectDB();
+
+    // Sets collection from database
+    const collection = database.collection('gui_event');
+
+    // Find elements and limits return to five with biggest id
+    return await collection
+      .find({})
+      .sort({ _id: -1 })
+      .limit(5)
+      .toArray();
+
+    // Error catch
+  } catch (err) {
+    console.error('Failed to fetch latest records:', err.message);
+    throw err;
+  }
+};
+
+
+// Exported functions for use in other files
+export { connectDB, getFiveLatest };
---
+++ index.js
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+import express from 'express';
+import { connectDB, getFiveLatest } from './database/db.js';
+
+// Setup express
+const app = express();
+
+// Setup base elements for app
+(async () => {
+
+  // Calls db connection function
+  await connectDB();
+
+  // Sets Port and returns port in console
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})();
+
+//////////////////////////////////////////////// Routing /////////////////////////////////////////
+
+// index page
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// latest page
+app.get('/latest', async (req, res) => {
+  try {
+    // Calls get 5 latest and return in json format on page
+    const data = await getFiveLatest();
+    res.json(data);
+
+    // Catch error
+  } catch {
+    res.status(500).send('Error fetching data');
+  }
+});
